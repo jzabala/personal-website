dc57700001017f8c1070fb23c2272a5701898d47
Johnny Zabala <jzabala.s@gmail.com>
2020-05-30 12:21:40 -0400

    refactor(onCreateNode): use getNode helper to get file node

--- src/internal/on-create-node.js
+++ src/internal/on-create-node.js
@@ -1,34 +1,29 @@
 const { languages, defaultLanguage } = require('../utils/i18n')
 
-function onCreateNode({ node, actions }) {
+function onCreateNode({ node, actions, getNode }) {
   const { createNodeField } = actions
-  //eslint-disable-next-line default-case
-  switch (node.internal.type) {
-    case 'Mdx': {
-      const { fileName, langKey } = getMetaFromFilePath(node.fileAbsolutePath)
-      createNodeField({
-        node,
-        name: 'langKey',
-        value: langKey,
-      })
-      createNodeField({
-        node,
-        name: 'url',
-        value: getUrl(langKey, fileName),
-      })
-      break
-    }
+  if (node.internal.type === 'Mdx') {
+    const fileNode = getNode(node.parent)
+    const { fileName, langKey } = getMetaFromFileName(fileNode.name)
+    createNodeField({
+      node,
+      name: 'langKey',
+      value: langKey,
+    })
+    createNodeField({
+      node,
+      name: 'url',
+      value: getUrl(langKey, fileName),
+    })
   }
 }
 
-function getMetaFromFilePath(path) {
-  const tokens = path.split(/.*\//)
-  const fullFileName = tokens[tokens.length - 1]
-  const [fileName, langKey] = fullFileName.split('.')
+function getMetaFromFileName(name) {
+  const [fileName, fileLangKey] = name.split('.')
   return {
     fileName,
-    langKey: languages.some(lang => lang === langKey)
-      ? langKey
+    langKey: languages.some(lang => lang === fileLangKey)
+      ? fileLangKey
       : defaultLanguage,
   }
 }
